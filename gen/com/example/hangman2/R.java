574fdc4da5e25c78ec84937bf9dc222f483a37cf
sparik <sparikhayrapetyan@gmail.com>
2014-08-23 03:51:26 +0400

    first commit

--- gen/com/example/hangman2/R.java
+++ gen/com/example/hangman2/R.java
@@ -39,6 +39,7 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher=0x7f020001;
         public static final int keyboard_button=0x7f020002;
         public static final int letter_shape=0x7f020003;
+        public static final int roundedbutton=0x7f020004;
     }
     public static final class id {
         public static final int action_settings=0x7f090013;
