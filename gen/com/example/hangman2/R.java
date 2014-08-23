101eed673da20626cbf4bd49acbc1fa0805f9de8
Serg Hov <sergey.hovakimyan@gmail.com>
2014-08-24 00:32:23 +0400

    changed the color of the keyboard

--- gen/com/example/hangman2/R.java
+++ gen/com/example/hangman2/R.java
@@ -49,27 +49,26 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int texture=0x7f02000b;
     }
     public static final class id {
-        public static final int action_settings=0x7f090014;
-        public static final int easyButton=0x7f09000d;
-        public static final int hardButton=0x7f09000f;
-        public static final int imageView1=0x7f09000a;
+        public static final int action_settings=0x7f090013;
+        public static final int easyButton=0x7f09000c;
+        public static final int hardButton=0x7f09000e;
         public static final int keyboardRow1=0x7f090001;
         public static final int letterContainer=0x7f090005;
         public static final int lifeCountContainer=0x7f090009;
-        public static final int linearLayout1=0x7f09000c;
-        public static final int mediumButton=0x7f09000e;
-        public static final int menuButton=0x7f090011;
+        public static final int linearLayout1=0x7f09000b;
+        public static final int mediumButton=0x7f09000d;
+        public static final int menuButton=0x7f090010;
         public static final int myKeyboard=0x7f090000;
         public static final int nextTmpButton=0x7f090008;
-        public static final int playAgainButton=0x7f090012;
+        public static final int playAgainButton=0x7f090011;
         public static final int questionContainer=0x7f090006;
         public static final int score=0x7f090007;
-        public static final int skipButton=0x7f09000b;
+        public static final int skipButton=0x7f09000a;
         public static final int tableRow2=0x7f090002;
         public static final int tableRow3=0x7f090003;
         public static final int tableRow4=0x7f090004;
-        public static final int textView=0x7f090013;
-        public static final int textView1=0x7f090010;
+        public static final int textView=0x7f090012;
+        public static final int textView1=0x7f09000f;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
